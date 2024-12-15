dbc23abc97728f1df13280a86a1cc3dbf95e328a
hata6502 <7702653+hata6502@users.noreply.github.com>
2024-12-16 00:27:25 +0900

    Update clarity.js

--- src/clarity.js
+++ src/clarity.js
@@ -9,4 +9,4 @@
   t.src = "https://www.clarity.ms/tag/" + i;
   y = l.getElementsByTagName(r)[0];
   y.parentNode.insertBefore(t, y);
-})(window, document, "clarity", "script", "pac0u1emft");
+})(window, document, "clarity", "script", "pef6dmyql9");
